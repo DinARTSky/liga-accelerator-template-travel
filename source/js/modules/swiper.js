5cf2b5125af695d53878f0dbd973706181ab4288
Динар Ташбулатов <tdinarf@mail.ru>
2023-12-10 18:30:24 +0500

    main

--- source/js/modules/swiper.js
+++ source/js/modules/swiper.js
@@ -1,6 +1,35 @@
-const initSwiper = new Swiper('.swiper');
+const initSwiper = new Swiper('.hero__slider', {
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    // Буллеты
+    clickable: true,
+  },
+});
 
-export default initSwiper();
+const initSwiperTours = new Swiper('.swiper-tours', {
+  // Navigation arrows
+  navigation: {
+    nextEl: '.tours__button--next',
+    prevEl: '.tours__button--prev',
+  },
+  // Количество слайдов для показа
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18,
+      initialSlide: 2,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      initialSlide: 1,
+    },
+  },
+});
+
+export default initSwiper(); initSwiperTours();
 
 
 /* const initSwiper = new Swiper('.swiper', {
