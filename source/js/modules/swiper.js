8ee177d1192cff74d71de323e728c75080d1ce18
Динар Ташбулатов <tdinarf@mail.ru>
2023-12-18 20:17:48 +0500

    main

--- source/js/modules/swiper.js
+++ source/js/modules/swiper.js
@@ -80,7 +80,81 @@ const initSwiperReviews = new Swiper('.reviews__slider', {
   },
 });
 
-export default initSwiper(); initSwiperTours(); initSwiperTrainers(); initSwiperReviews;
+const initSwiperAdvantages = new Swiper('.advantages__slider', {
+  // Navigation arrows
+  navigation: {
+    nextEl: '.advantages__button--next',
+    prevEl: '.advantages__button--prev',
+  },
+  loop: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+    },
+    1440: {
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+    },
+  },
+});
+
+// сначала пишем необходимый брейкпоинт
+let desktop = window.matchMedia('(min-width: 1440px)'); // сами подберите нужный брейкпоинт
+
+if (desktop.matches) {
+  const initSwiperAdvantages = new Swiper('.advantages__slider', {
+    // Navigation arrows
+    navigation: {
+      nextEl: '.advantages__button--next',
+      prevEl: '.advantages__button--prev',
+    },
+    loop: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+      },
+      1440: {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+      },
+    },
+  });
+} else {
+// если слайдер не в брейкпоинте
+  swiper.destroy(); // то уничтожаем его
+}
+
+const initSwiperGallery = new Swiper('.gallery__slider', {
+  // Navigation arrows
+  navigation: {
+    nextEl: '.gallery__button--next',
+    prevEl: '.gallery__button--prev',
+  },
+  loop: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+    },
+    1440: {
+      slidesPerView: 'auto',
+      spaceBetween: 5,
+    },
+  },
+});
+
+export default initSwiper(); initSwiperTours(); initSwiperTrainers(); initSwiperReviews(); initSwiperAdvantages(); initSwiperGallery();
 
 
 /* const initSwiper = new Swiper('.swiper', {
